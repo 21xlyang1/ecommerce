b8faa71688c76f3d71dfce997a87b598a4c110ec
321 <3215927286@qq.com>
2023-12-05 18:16:00 +0800

    lly 12/5

---
+++ src/components/commentBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="MainDiv">
+        <div class="Box">
+            <span>这是评论的box</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+    .Box{
+        background-color: #f00;
+        // height: 50px;
+    }
+    span{
+        background-color: #ff0;
+        // height: 100px;
+    }
+</style>
--- src/pages/commentManage.vue
+++ src/pages/commentManage.vue
@@ -1,15 +1,54 @@
+<!-- 评论管理页面 -->
 <template>
-  <div>
-    评论管理页
+  <div class="M">
+    <div class="body">
+      <span>评论管理页面</span>
+      <commentBox />
+      <commentBox />
+    </div>
+    <commentBox />
   </div>
 </template>
+
 <script>
-  export default {
-    name:"",
-  }
+// 引入组件
+import commentBox from "../components/commentBox.vue";
+
+// 定义vm
+export default {
+  components: {
+    commentBox, // 注册评论管理的box组件
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+// <template>的独一无二的div
+.M{
+  display: flex;                  // 设置页面的组件都水平居中
+  justify-content: center;        // 水平居中
+  flex-wrap: wrap;                // 允许换行
+  // background-color: brown;
+  background-color: #dcdcdc;
+  height: 100vh;                  // 设置高度，取消则为自定义高度
+  padding: 10px;                  // 设置内边距
+}
+// 最大的div，用来存放所有的组件，名字定位body
+.body {
+  display: flex;                  // 设置页面的组件都水平居中
+  // justify-content: center;        // 水平居中
+  align-items: center;            // 水平居中
+  flex-direction: column;         // 垂直方向排列
 
+  padding: 10px;
+  background-color: aqua;
+  width: 100%;
+}
+span {
+  // width: 100%; // 使组件充满 .body 的宽度
+  box-sizing: border-box;
+  background-color: #fff;
+  // margin: 10px;
+}
 </style>
 

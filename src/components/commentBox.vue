746dd1fd8b70e7111198f155306deb0705c490b6
321 <3215927286@qq.com>
2023-12-19 13:39:35 +0800

    lly 12/5

--- src/components/commentBox.vue
+++ src/components/commentBox.vue
@@ -1,22 +1,116 @@
 <template>
     <div class="MainDiv">
         <div class="Box">
-            <span>这是评论的box</span>
+            <div class="checkbox-Box"><input type="checkbox"></div>
+            <div class="author">
+                <div class="up">
+                    <div class="l"><img src="../assets/img/默认头像.svg" alt="头像" width="80px"></div>
+                    <div class="r">
+                        <div class="a0"><span>{{ name }}</span></div>
+                        <div class="a1"><span>{{ email }}</span></div>
+                        <div class="a2"><span>{{ wechat }}</span></div>
+                        <div class="a3"><span>{{ photonumber }}</span></div>
+                    </div>
+                </div>
+                <div class="down">
+                    <div>不知道放啥</div>
+                </div>
+            </div>
+            <div class="comment">
+                {{ comments }}
+            </div>
+            <div class="reply">
+                {{ reply }}
+            </div>
+            <div class="submission-time">
+                {{ submission }}
+            </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-
+    data() {
+        return {
+            name: "Peanut",
+            email: "123456789@123123.com",
+            wechat: "123456789abcd",
+            photonumber: "123456789abcdefg",
+            comments: "第一条评论",
+            reply: "这是回复",
+            submission: "1234:12:12 12:12"
+        };
+    },
 }
 </script>
 <style lang="scss" scoped>
-    .Box{
-        background-color: #f00;
-        // height: 50px;
+.MainDiv {
+    border: 1px solid #ccc;        /* 设置通用的边框 */
+    border-top: none;                   /* 上方边框不显示 */
+}
+
+.checkbox-Box {
+    // background-color: black;         // 复选框的div颜色
+    background-color: rgba(253, 247, 241, 1); // 复选框的div颜色
+    display: flex;
+    justify-content: center;            // 水平居中
+    align-items: flex-start;            // 垂直居中
+    padding-top: 12px;
+}
+
+.checkbox-Box input[type="checkbox"] {
+    width: 18px;  /* 设置复选框的宽度 */
+    height: 18px; /* 设置复选框的高度 */
+    cursor: pointer;  // 悬浮改为手型
+}
+
+.Box {
+    display: grid; // 评论列表的标题设置为grid布局
+    grid-template-columns: 35px 300px 1fr 250px 150px; // 四列，左右布局
+    background-color: rgba(253, 247, 241, 1);
+    height: 150px;
+}
+
+.Box .author {
+    display: grid;                      // 评论列表的标题设置为grid布局
+    grid-template-rows: 120px 1fr;        // 两行，上下布局
+    // background-color: red;           // 测试的时候 作者栏的div颜色
+}
+.author .up {
+    // background-color: brown;
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    .l {
+        padding-top: 10px;
     }
-    span{
-        background-color: #ff0;
-        // height: 100px;
+    .r {
+        padding-left: 10px;
+        padding-top: 7px;
+        .a0 span {
+            font-size: 1.25rem;
+            cursor: pointer;            // 改变为悬停手型
+        }
+        .a1 span, .a2 span, .a3 span {
+            font-size: 0.95rem;
+            color: #00f;
+            cursor: pointer;            // 改变为悬停手型
+            // background-color: cyan;
+        }
     }
+}
+.author .down {
+    // background-color: darkorchid;
+}
+
+.Box .comment {
+    // background-color: yellow;
+}
+
+.Box .reply {
+    // background-color: blue;
+}
+
+.Box .submission-time {
+    // background-color: green;
+}
 </style>
--- src/pages/commentManage.vue
+++ src/pages/commentManage.vue
@@ -1,12 +1,74 @@
 <!-- 评论管理页面 -->
 <template>
   <div class="M">
+    <div class="spanDiv"><span>评论</span></div>
     <div class="body">
-      <span>评论管理页面</span>
-      <commentBox />
-      <commentBox />
+      <div class="functionBox">
+        <div class="l">
+          <div class="box_l">
+            <div class="all">全部 ({{ data_all }})</div>
+            |
+            <div class="mine">我的 ({{ data_mine }})</div>
+            |
+            <div class="pending">待审 ({{ data_pending }})</div>
+            |
+            <div class="permitted">已批准 ({{ data_permitted }})</div>
+            |
+            <div class="waste">垃圾 ({{ data_waste }})</div>
+            |
+            <div class="recycle">回收站 ({{ data_recycle }})</div>
+          </div>
+          <div class="box_btu">
+            <select>
+              <option value="" disabled selected hidden>批量操作</option>
+              <option value="">选项一</option>
+              <option value="">选项二</option>
+              <option value="">选项三</option>
+            </select>
+            <div class="app">应用</div>
+            <select>
+              <option value="" disabled selected hidden>全部评论类型</option>
+              <option value="">选项一</option>
+              <option value="">选项二</option>
+              <option value="">选项三</option>
+            </select>
+            <div class="classify">筛选</div>
+          </div>
+        </div>
+        <div class="r">
+          <div class="box-r">
+            <div class="text">
+              <span>共{{ data_num }}个数据</span>
+            </div>
+            <el-pagination
+              class="pagination"
+              background
+              layout="prev, pager, next"
+              :total="totalComments"
+              :page-size="commentsPerPage"
+              v-model="currentPage"
+              pager-count="5"
+              hide-on-single-page="true"
+            >
+            </el-pagination>
+          </div>
+        </div>
+      </div>
+      <div class="commentBox">
+        <div class="navigation">
+          <div class="checkbox-Box"><input type="checkbox" /></div>
+          <div class="a">作者</div>
+          <div class="b">评论</div>
+          <div class="c">回复至</div>
+          <div class="d">提交于</div>
+        </div>
+        <commentBox />
+        <commentBox />
+        <commentBox />
+        <commentBox />
+        <commentBox />
+      </div>
     </div>
-    <commentBox />
   </div>
 </template>
 
@@ -19,36 +81,220 @@ export default {
   components: {
     commentBox, // 注册评论管理的box组件
   },
+  data() {
+    return {
+      totalComments: 30, // 实际上的评论数量
+      commentsPerPage: 5, // 每一页的评论数量
+      currentPage: 1, // 当前的页面编号
+      data_num: 2236, // 数据有多少
+      data_all: 27657,
+      data_mine: 3,
+      data_pending: 27651,
+      data_permitted: 6,
+      data_waste: 26,
+      data_recycle: 0,
+    };
+  },
+  computed: {},
 };
 </script>
 
 <style lang="scss" scoped>
 // <template>的独一无二的div
-.M{
-  display: flex;                  // 设置页面的组件都水平居中
-  justify-content: center;        // 水平居中
-  flex-wrap: wrap;                // 允许换行
-  // background-color: brown;
-  background-color: #dcdcdc;
-  height: 100vh;                  // 设置高度，取消则为自定义高度
-  padding: 10px;                  // 设置内边距
+.M {
+  // background-color: black;
+  height: 100vh; // 设置高度，取消则为自定义高度
+  padding: 30px; // 设置内边距
 }
-// 最大的div，用来存放所有的组件，名字定位body
-.body {
-  display: flex;                  // 设置页面的组件都水平居中
-  // justify-content: center;        // 水平居中
-  align-items: center;            // 水平居中
-  flex-direction: column;         // 垂直方向排列
 
-  padding: 10px;
-  background-color: aqua;
+// 最大的用来操作的div，用来存放所有的组件，名字定位body
+.body {
+  display: grid; // 设置为grid布局
+  // grid-auto-rows: minmax(100px, auto); // 设置每行的高度最小100px最大auto
+  // gap: 20px; // 列之间的间隙
+
+  // padding: 10px;                     // 设定body的内边距
   width: 100%;
 }
-span {
-  // width: 100%; // 使组件充满 .body 的宽度
-  box-sizing: border-box;
+
+.body .functionBox {
+  // background-color: #fff; // 设定功能div的背景颜色
+  width: 100%; // 设定功能div的宽度为100%
+  height: 80px; // 设定功能div的高度为固定高度
+
+  box-sizing: border-box; // 边框包含在盒模型内
+  // border: 1px solid #ccc; // 设置边框为红色，1像素宽度 */
+
+  display: grid;
+  grid-template-columns: 1fr 500px; // 两列，左右布局
+
+  .l {
+    // background-color: coral;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    .box_l {
+      // background-color: aqua;
+      display: flex;
+      padding-bottom: 10px;
+      // padding-left: 10px;
+      .mine,
+      .pending,
+      .permitted,
+      .waste {
+        padding-right: 10px;
+        padding-left: 10px;
+      }
+      .all {
+        padding-right: 10px;
+      }
+      .recycle {
+        padding-left: 10px;
+      }
+    }
+    .box_btu {
+      // background-color: cyan;
+      padding-bottom: 10px;
+      // padding-left: 10px;
+      display: flex;
+      .app, .classify {
+        cursor: pointer;
+        box-sizing: border-box;
+        background-color: #fff;
+        border: 1px solid #000; // 设置边框为红色，1像素宽度
+        width: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .app {
+        margin-right: 10px;
+        margin-left: 10px;
+      }
+      .classify {
+        margin-left: 10px;
+      }
+      select {
+        width: 120px;
+      }
+    }
+  }
+
+  .r {
+    // background-color: blueviolet;
+    position: relative;
+    .box-r {
+      position: absolute;
+      right: 5px;
+      bottom: 5px;
+
+      display: grid;
+      grid-template-columns: 1fr auto; /* 左右两列，第一列占据剩余空间，第二列自适应宽度 */
+      gap: 10px; /* 根据需要设置列之间的间隔 */
+      .text {
+        // background-color: brown;
+        font-size: 0.95rem;
+        display: flex;
+        // justify-content: space-between; /* 文字水平居中，span 右对齐 */
+        align-items: center; /* 文字垂直居中 */
+        span {
+        }
+      }
+      .pagination {
+        // background-color: chartreuse;
+        bottom: 10px;
+        right: 10px;
+      }
+    }
+  }
+}
+
+.body .commentBox {
+  background-color: #fff; // 设定评论div的背景颜色
+  width: 100%; // 设定评论div的宽度为100%
+  // padding: 10px;                     // 设定评论div的内边距
+
+  display: grid; // 设置为grid布局
+  grid-auto-rows: minmax(0px, auto); // 设置每行的高度，自动调整
+
+  box-sizing: border-box; // 边框包含在盒模型内
+}
+
+.commentBox .navigation {
   background-color: #fff;
-  // margin: 10px;
+  font-size: 1.2rem; // 评论列表的标题字体
+  display: grid; // 评论列表的标题设置为grid布局
+  grid-template-columns: 35px 300px 1fr 250px 150px; // 五列，左右布局
+
+  // padding-top: 5px;
+  // padding-bottom: 5px;
+  width: 100%;
+
+  border: 1px solid #ccc;
+  /* 设置通用的边框 */
+}
+
+.checkbox-Box {
+  // background-color: black;
+  // background-color: rgba(253, 247, 241, 1); // 复选框的div颜色
+  display: flex;
+  justify-content: center; // 水平居中
+  align-items: flex-start; // 垂直居中
+  padding-top: 10px;
+}
+
+.checkbox-Box input[type="checkbox"] {
+  width: 18px;
+  /* 设置复选框的宽度 */
+  height: 18px;
+  /* 设置复选框的高度 */
+  cursor: pointer; // 悬浮改为手型
+}
+
+.navigation .a {
+  // background-color: red;
+  color: #00f;
+
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.navigation .b {
+  // background-color: yellow;
+  color: #00f;
+
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.navigation .c {
+  // background-color: blue;
+  color: #00f;
+
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.navigation .d {
+  // background-color: green;
+  color: #00f;
+
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.spanDiv {
+  box-sizing: border-box;
+  // background-color: #fff;
+  font-size: 1.5rem;
+  // padding-left: 10px;
+
+  box-sizing: border-box;
+  /* 边框包含在盒模型内 */
+  // border: 1px solid #ccc;
+  /* 设置边框为红色，2像素宽度 */
+
+  margin-bottom: 10px;
 }
 </style>
 

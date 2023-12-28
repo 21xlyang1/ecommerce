c28c21de448573a2ae8d3797919540656af4cd93
HaoeaHaokuna <93109517+HaoeaHaokuna@users.noreply.github.com>
2023-12-29 04:03:50 +0800

    Update userManage.vue

--- src/pages/userManage.vue
+++ src/pages/userManage.vue
@@ -57,6 +57,9 @@
           <label for="edit-phone">号码:</label>
           <input type="text" id="edit-phone" v-model="editedUser.phone" required>
 
+          <label for="edit-password">密码：</label>
+          <input type="text" id="edit-password" v-model="editedUser.password" required>
+
           <label for="edit-role">角色:</label>
           <select id="edit-role" v-model="editedUser.role" required>
             <option value="管理员">管理员</option>
@@ -84,6 +87,9 @@
 
           <label for="add-phone">号码:</label>
           <input type="text" id="add-phone" v-model="newUser.phone" required>
+
+          <label for="add-password">密码：</label>
+          <input type="text" id="add-password" v-model="newUser.password" required>
 
           <label for="add-role">角色:</label>
           <select id="add-role" v-model="newUser.role" required>
@@ -122,9 +128,9 @@ export default {
         role: ""
       },
       editingUser: false,
-      editedUser: { id: null, username: "", email: "", phone: "", role: "" },
+      editedUser: { id: null, username: "", email: "", phone: "", password:"",role: "" },
       addingUser: false, // 添加用户模态框的显示状态
-      newUser: { username: "", email: "", phone: "", role: "" }, // 新用户的信息
+      newUser: { username: "", email: "", phone: "",password:"", role: "" }, // 新用户的信息
     };
   },  
   mounted() {
